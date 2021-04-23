84abff0c6e0f276ae7c277eb46450fb4e706b6e6
unknown <nilimawadal1@gmail.com>
2021-04-23 20:26:16 +0530

    [Nilima]Added Contact List

---
+++ Address-Book-Program/Address-Book-Program/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_Program
+{/// <summary>
+/// Creating Class for Adding List and Data
+/// </summary>
+    class Person
+    {
+        //Get And Set method to getting and Setting Elements
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public long Zip { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+        //Creating List to add Person data
+        public static List<Person> People = new List<Person>();
+
+    }
+}
--- Address-Book-Program/Address-Book-Program/Program.cs
+++ Address-Book-Program/Address-Book-Program/Program.cs
@@ -5,12 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Address_Book_Program
-{
+{/// <summary>
+/// Main Method To call Methods
+/// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("******Welcome To the Address Book*****");
+            //Creating Object of Person 
+            Person person = new Person();
+            //Taking Various Input From User
+            Console.WriteLine("Enter First Name :");
+            person.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            person.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            person.Address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            person.City = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            person.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            person.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            person.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email Address :");
+            person.Email = Console.ReadLine();
+            //Adding person Details in List
+            Person.People.Add(person);
+            //Initialized For Loop for Printing Each Details of person
+            foreach (var i in Person.People)
+            {
+                Console.WriteLine(i.FirstName);
+                Console.WriteLine(i.LastName);
+                Console.WriteLine(i.Address);
+                Console.WriteLine(i.City);
+                Console.WriteLine(i.State);
+                Console.WriteLine(i.Zip);
+                Console.WriteLine(i.Email);
+            }//End of For Loop
+            Console.Read();
         }
+        
+       
     }
 }
