9f199516b64835f3aa723fc72155ea607b48e881
unknown <nilimawadal1@gmail.com>
2021-04-23 20:12:30 +0530

    [Nilima]Added Data

---
+++ Address-Book-Program/Address-Book-Program/ContactDetails.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_Program
+{/// <summary>
+/// Contact Details
+/// </summary>
+    class ContactDetails//default constructor
+    {
+        //Declaring Variables
+        string firstName;
+        string lastName;
+        string address;
+        string city;
+        string state;
+        string zip;
+        string phoneNumber;
+        string email;
+
+        //Creating method Constructor 
+        public ContactDetails(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            //Instance Variables
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        //Get method for getting the Elements
+        public string getName()
+        {
+            return this.firstName;
+        }
+        public string getlastName()
+        {
+            return this.lastName;
+        }
+        public string getaddress()
+        {
+            return this.address;
+        }
+        public string getcity()
+        {
+            return this.city;
+        }
+        public string getstate()
+        {
+            return this.state;
+        }
+        public string getzip()
+        {
+            return this.zip;
+        }
+        public string getphone()
+        {
+            return this.phoneNumber;
+        }
+        public string getemail()
+        {
+            return this.email;
+        }
+        //Adding Tostring method To storing data 
+        public string Tostring()
+        {
+            return "First Name:" + firstName + "\nLast Name:" + lastName + "\nAddress:" + address + "\nState:" + state + "\nCity:" + city + "\nZip:" + zip + "\nPhoneNumber:" + phoneNumber;
+
+        }
+
+    }
+}
--- Address-Book-Program/Address-Book-Program/Program.cs
+++ Address-Book-Program/Address-Book-Program/Program.cs
@@ -5,12 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Address_Book_Program
-{
+{/// <summary>
+/// Main Method Adding Data Manually
+/// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("******Welcome To the Address Book*****");
+            Console.WriteLine("*****WelCome TO Address Book*****");
+            //Creating Object
+            ContactDetails person = new ContactDetails("Nilima", "Wadal", "Chhotta", "Akola", "Maharashtra", "444101", "9130264839", "nilimawadal@gmail.com");
+            //Calling all Data
+            Console.WriteLine(person.getName());
+            Console.WriteLine(person.getlastName());
+            Console.WriteLine(person.getaddress());
+            Console.WriteLine(person.getcity());
+            Console.WriteLine(person.getstate());
+            Console.WriteLine(person.getzip());
+            Console.WriteLine(person.getphone());
+            Console.WriteLine(person.getemail());
+            Console.Read();
+
         }
     }
 }
