5725b84f90a2d71d0a466391177b06d0a0a570cc
unknown <nilimawadal1@gmail.com>
2021-04-23 21:07:06 +0530

    [Nilima] Added Deleting Contact From The List

---
+++ Address-Book-Program/Address-Book-Program/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_Program
+{/// <summary>
+/// Creating Person Class Using Get,Set Method
+/// </summary>
+    class Person
+    {
+        // For Getting And Setting Elements
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public long Zip { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public static List<Person> People = new List<Person>();
+
+        //Creating Method 
+        public static void AddPerson()
+        {
+            //Creating Object of Person 
+            Person person = new Person();
+            //Taking Various Input From User
+            Console.WriteLine("Enter First Name :");
+            person.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            person.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            person.Address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            person.City = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            person.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            person.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            person.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email Address :");
+            person.Email = Console.ReadLine();
+            //Adding person Details in List
+            Person.People.Add(person);
+
+        }
+    }
+}
--- Address-Book-Program/Address-Book-Program/Program.cs
+++ Address-Book-Program/Address-Book-Program/Program.cs
@@ -5,12 +5,54 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Address_Book_Program
-{
+{/// <summary>
+/// Main Method 
+/// </summary>
     class Program
-    {
+    {/// <summary>
+     /// Calling Methods 
+     /// </summary>
+     /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("******Welcome To the Address Book*****");
+            Console.WriteLine("*****WelCome To The Address-Book*****");
+            //Calling AddPerson Method
+            Person.AddPerson();
+            Person.AddPerson();
+            //Initialized For Loop for Printing Each Details of person
+            foreach (var i in Person.People)
+            {
+                //Print And Store Data
+                Console.WriteLine(i.FirstName + "  " + i.LastName + "  " + i.Address + "  " + i.City + "  " + i.State + "  " + i.Zip + "  " + i.State + "  " + i.Email);
+            }
+            //WriteLine for UserInputs
+            Console.WriteLine("Please Enter the Name You Want to Edit : ");
+            string userinput = Console.ReadLine();
+            //Initialized For Loop for Editing with FirstName
+            foreach (var j in Person.People)
+            {
+                //Initialized if Loop For Finding Name
+                //If True Then Edit
+                if (j.FirstName == userinput)
+                {
+                    Person.People.Remove(j);
+                    break;
+
+                }
+                else
+                {
+                    //If False then Print
+                    Console.WriteLine("Such Entries Are not Found");
+                }//End of If ElseLoop
+
+            }//End of If Loop
+            //After Editing Contact Will Be
+            foreach (var i in Person.People)
+            {
+                Console.WriteLine(i.FirstName + "  " + i.LastName + "  " + i.Address + "  " + i.City + "  " + i.State + "  " + i.Zip + "  " + i.State + "  " + i.Email);
+            }
+            //End of If Loop
+            Console.Read();
         }
     }
 }
